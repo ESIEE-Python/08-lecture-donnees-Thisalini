807eb749a874fde562dae9ee321b2097d3df5894
Thisalini <thisalini.ravinthiran@edu.esiee.fr>
2024-10-05 18:02:56 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,47 +1,78 @@
+""" travailler sur les fichiers """
 #### Imports et définition des variables globales
 import random
-
 FILENAME = "listes.csv"
 
 #### Fonctions secondaires
-
+""" travailler sur les listes apporté par les fichiers"""
 def read_data(filename):
     """retourne le contenu du fichier <filename>
-
     Args:
         filename (str): nom du fichier à lire
-
     Returns:
         list: le contenu du fichier (1 list par ligne)
     """
-    l = []
+    with open(filename, mode='r', encoding='utf8') as file:
+        # Utilise readlines() pour lire toutes les lignes du fichier
+        r = file.readlines()
+
+        # Pour chaque ligne, enlève les espaces inutiles et convertit les éléments en entier
+        l = [list(map(int, line.split(';'))) for line in r]
+
     return l
 
-def get_list_k(data, k):
-    l = []
-    return l
+
+def get_list_k(l,k):
+    """retourne la kième liste
+    Args: prend en argument la liste de listes retournée par read_data() et un indice k
+
+    Return : retourne la kième liste
+    """
+    return l[k]
+print(get_list_k(read_data(FILENAME),8))
 
 def get_first(l):
-    return None
+    """"retourne le premier élément de cette liste 
+    Arg : prend en argument une liste"""
+    return l[0]
+print(get_first(get_list_k(read_data(FILENAME),8)))
 
 def get_last(l):
-    return None
+    """ donne le dernier élément de la liste
+    arg : liste l
+    """
+    n = len(l)
+    return l[n-1]
+print(get_last(get_list_k(read_data(FILENAME),8)))
+
 
 def get_max(l):
-    return None
+    """ donne le max de la liste
+    arg : liste l
+    """
+    return max(l)
+print(get_max(get_list_k(read_data(FILENAME),8)))
 
 def get_min(l):
-    return None
+    """ donne le min de la liste
+    arg : liste l
+    """
+    return min(l)
+print(get_min(get_list_k(read_data(FILENAME),8)))
 
 def get_sum(l):
-    return None
+    """ donne la somme des élément de la liste 
+    arg : liste l
+    """
+    return sum(l)
+print(get_sum(get_list_k(read_data(FILENAME),8)))
 
 
 #### Fonction principale
 
 
 def main():
-    pass
+    """faire appel aux fonctions secondaires pour vérifier leur bon fonctionnement."""
     # data = read_data(FILENAME)
     # for i, l in enumerate(data):
     #     print(i, l)
